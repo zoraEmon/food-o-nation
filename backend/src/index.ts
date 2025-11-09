b83aef6970c008878a4f414e2f7dc36d051ea5c1
zoraEmon <125435545+zoraEmon@users.noreply.github.com>
2025-11-09 17:47:23 +0800

    Setup of frontend and backend

---
+++ backend/src/index.ts
@@ -0,0 +1,16 @@
+import "dotenv/config"; 
+
+import express from "express";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ message: "Starter Code lang to, Pa Sad O++ :L" });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
---
+++ backend/src/prisma-test.ts
@@ -0,0 +1,17 @@
+//Checking if Prisma is working properly
+
+import "dotenv/config";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const rows = await prisma.try.findMany();
+  console.log("Rows:", rows);
+}
+
+main()
+  .catch((e) => console.error(e))
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
